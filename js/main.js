06585728572542168c95c03a6a993f6868d94c84
keyBash <theilya777@gmail.com>
2023-05-13 17:49:28 +0300

    working model of user and machine with console.log

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
-let arr =  ['rock','paper','scissor']
+function getComputerChoice() {
+  const newArray = ["rock", "paper", "scissors"];
+  let computerSelection = newArray[Math.floor(Math.random() * 3)];
+  return computerSelection;
+}
+let playerSelection = prompt("rock,paper,scissors?");
+let computerSelection = getComputerChoice();
+let score = 0;
 
-
-
-
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  
+function getResult() {
+  if (playerSelection === computerSelection) {
+    score = 0;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    score = 1;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    score = 1;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    score = 1;
+  } else {
+    score = -1;
   }
-  function computerSelection(arr){
-        let rndArr = Math.floor(Math.random()*arr.length)
-        return arr[rndArr]
+  return score;
 }
 
+function getWinner() {
+  let lol = getResult();
+  if (score == 0) {
+    return console.log(`${playerSelection} vs ${computerSelection} its a tie!`);
+  } else if (score == 1) {
+    return console.log(`${playerSelection} vs ${computerSelection} You win`);
+  } else score == -1;
+  {
+    return console.log(`${playerSelection} vs ${computerSelection} You loz`);
+  }
+}
 
-console.log(computerSelection(arr))
- 
-  const playerSelection = "rock";
-/*   const computerSelection = getComputerChoice(); */
-  console.log(playRound(playerSelection, computerSelection(arr)));
-
-
-
-
-
+getWinner();
